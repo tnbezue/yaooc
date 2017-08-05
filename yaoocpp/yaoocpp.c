58c0b4bad54bed9bcac6e45abcde12cc4e3daa61
Terry Bezue <tnbezue@gmail.com>
2017-08-05 14:11:40 -0400

    Updated due to renamed files

--- yaoocpp/yaoocpp.c
+++ yaoocpp/yaoocpp.c
@@ -7,6 +7,32 @@
 #include <ctype.h>
 #include "yaooc_definition_parser.h"
 #include <yaooc/garbage_bag.h>
+#include <getopt.h>
+
+#define YAOOCPP_MAJOR 1
+#define YAOOCPP_MINOR 0
+void print_info()
+{
+  printf("YAOOCPP -- Yet Another Object Oriented C PreProcessor\n");
+}
+
+void print_version()
+{
+  print_info();
+  printf("Version: %d.%d",YAOOCPP_MAJOR,YAOOCPP_MINOR);
+}
+
+void print_help()
+{
+  print_info();
+  printf("Syntax: yaoocpp [ -h | --help ]\n");
+  printf("        yaoocpp [ -v | --version ]\n");
+  printf("        yaoocpp file.yaooc\n");
+  printf("Where:\n");
+  printf("  -h|--help -- prints this help\n");
+  printf("  -v|--version -- prints version information\n");
+  printf("  file.yaooc -- preprocesses file.yaooc into file.h.template and file.c.template\n");
+}
 
 char *root_name(const char* fname_in)
 {
@@ -62,21 +88,48 @@ void write_source(const char* root,item_definition_shared_ptr_vector_const_point
   GB_EXIT;
 }
 
+static struct option long_options[] =
+  {
+    {"version", no_argument,       0, 'v'},
+    {"help",   no_argument,      0, 'h'},
+    {0, 0, 0, 0}
+  };
+
+void parse_options(int argc,char* argv[])
+{
+  int c;
+  int option_index = 0;
+  c = getopt_long (argc, argv, "vh",long_options, &option_index);
+  switch(c) {
+    case -1:
+      break;
+
+    case 'v':
+      print_version();
+      exit(0);
+
+    case 'h':
+    default:
+      print_help();
+      exit(0);
+
+  }
+}
+
 int main(int argc,char* argv[])
 {
   GB_INIT;
-	setbuf(stdout,NULL);
-	setbuf(stderr,NULL);
+  parse_options(argc,argv);
+  if(optind == argc) {
+    print_help();
+    return 0;
+  }
   yaooc_definition_parser_pointer ydp=GB_SAVE(new(yaooc_definition_parser));
   int i;
-  item_definition_shared_ptr_vector_pointer classes; //=new(class_definition_vector_ti);
+  item_definition_shared_ptr_vector_pointer classes;
   for(i=1;i<argc;i++) {
     classes=GB_SAVE(M(ydp,parse_file,argv[i]));
     if(classes != NULL && M(classes,size) > 0) {
-/*      item_definition_shared_ptr_vector_const_iterator j;
-      CFOR_EACH(j,classes) {
-        M(M(j,cget),dump,stdout,0);
-      }*/
       char* root=GB_SAVE(root_name(argv[i]));
       write_header(root,classes);
       write_source(root,classes);
