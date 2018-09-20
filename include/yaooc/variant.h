f4ed089c6568e5b5c918608936e3fb50e2fbecb5
Terry Bezue <tnbezue@gmail.com>
2018-09-20 07:31:19 -0400

    Added variant

---
+++ include/yaooc/variant.h
@@ -0,0 +1,63 @@
+#ifndef __VARIANT_INCLUDED__
+#define __VARIANT_INCLUDED__
+
+/* Begin YAOOCPP output */
+
+#include <yaooc/object.h>
+#include <yaooc/stream.h>
+
+typedef enum { VAR_NONE, VAR_CHAR, VAR_UCHAR, VAR_SHORT, VAR_USHORT, VAR_INT, VAR_UINT, VAR_LONG, VAR_ULONG,
+			VAR_DOUBLE,VAR_CHAR_PTR,VAR_VOID_PTR,VAR_OBJECT} yaooc_variant_type_t;
+/*
+  Class Definition for yaooc_variant
+*/
+yaooc_class_table(yaooc_variant) {
+  yaooc_object_class_table_t;
+  yaooc_variant_type_t (*type)(const_pointer);
+  void (*set)(pointer, yaooc_variant_type_t, const void*);
+  const void* (*value)(const_pointer);
+};
+#define yaooc_variant_parent_class_table ((yaooc_object_class_table_t*)(yaooc_variant_class_table.parent_class_table_))
+
+yaooc_class_instance(yaooc_variant) {
+  yaooc_object_class_instance_t;
+  yaooc_variant_type_t type_;
+  union {
+  char ch_;
+  unsigned char uch_;
+  short sh_;
+  unsigned short ush_;
+  int int_;
+  unsigned int uint_;
+  long long long_;
+  unsigned long long ulong_;
+  double dbl_;
+  char* chr_ptr_;
+	void* void_ptr_;
+  yaooc_object_t* obj_;
+  };
+};
+
+yaooc_class(yaooc_variant);
+
+/* Type Info Prototypes for yaooc_variant */
+void yaooc_variant_default_ctor(pointer);
+void yaooc_variant_dtor(pointer);
+void yaooc_variant_copy_ctor(pointer,const_pointer);
+void yaooc_variant_assign(pointer,const_pointer);
+
+/* Constructors prototypes for yaooc_variant */
+
+/* Table prototypes for yaooc_variant */
+#define yaooc_variant_isa yaooc_object_isa
+#define yaooc_variant_swap yaooc_object_swap
+yaooc_variant_type_t yaooc_variant_type(const_pointer);
+void yaooc_variant_set(pointer, yaooc_variant_type_t,const void*);
+const void* yaooc_variant_value(const_pointer);
+
+/* Protected prototypes for yaooc_variant */
+
+/* End YAOOCPP output */
+
+
+#endif
---
+++ src/variant.c
@@ -0,0 +1,182 @@
+/* Begin YAOOCPP output */
+
+#include <yaooc/variant.h>
+#include <string.h>
+
+/* Private variables implementation for yaooc_variant */
+
+/* Private methods prototypes for yaooc_variant */
+
+/* Type Info implemmentation for yaooc_variant */
+void yaooc_variant_default_ctor(pointer p)
+{
+  yaooc_variant_pointer this=p;
+  this->type_=VAR_NONE;
+	this->dbl_=0;
+}
+
+void yaooc_variant_dtor(pointer p)
+{
+  yaooc_variant_pointer this=p;
+	switch(this->type_) {
+  	case VAR_NONE:
+  	case VAR_CHAR:
+  	case VAR_UCHAR:
+  	case VAR_SHORT:
+  	case VAR_USHORT:
+  	case VAR_INT:
+  	case VAR_UINT:
+  	case VAR_LONG:
+  	case VAR_ULONG:
+  	case VAR_DOUBLE:
+			break;
+
+  	case VAR_CHAR_PTR:
+		case VAR_VOID_PTR:
+			free(this->chr_ptr_);
+  		break;
+
+  	case VAR_OBJECT:
+  		delete(this->obj_);
+  		break;
+  }
+	yaooc_variant_default_ctor(this);
+}
+
+void yaooc_variant_copy_ctor(pointer p,const_pointer s)
+{
+  yaooc_variant_default_ctor(p);
+  yaooc_variant_assign(p,s);
+}
+
+void yaooc_variant_assign(pointer p,const_pointer s)
+{
+  yaooc_variant_pointer this=p;
+  yaooc_variant_const_pointer src=s;
+	yaooc_variant_set(this,src->type_,yaooc_variant_value(src));
+}
+
+/* Constructors implementation for yaooc_variant */
+
+/* Private methods implementation for yaooc_variant */
+
+/* Protected implementation for yaooc_variant */
+
+/* Table implementation for yaooc_variant */
+yaooc_variant_type_t yaooc_variant_type(const_pointer p)
+{
+  yaooc_variant_const_pointer this=p;
+  return this->type_;
+}
+
+void yaooc_variant_set(pointer p,yaooc_variant_type_t t,const void* value)
+{
+  yaooc_variant_pointer this=p;
+	yaooc_variant_dtor(this);
+	switch(t) {
+		case VAR_NONE:
+			break;
+
+		case VAR_CHAR:
+		case VAR_UCHAR:
+			this->ch_=*(const char*)value;
+			break;
+
+		case VAR_SHORT:
+		case VAR_USHORT:
+			this->sh_=*(const short*)value;
+			break;
+
+		case VAR_INT:
+		case VAR_UINT:
+			this->int_=*(const int*)value;
+			break;
+
+		case VAR_LONG:
+		case VAR_ULONG:
+			this->long_=*(const long long*)value;
+			break;
+
+		case VAR_DOUBLE:
+			this->dbl_=*(const double*)value;
+			break;
+
+		case VAR_CHAR_PTR:
+			this->chr_ptr_=strdup(value);
+			break;
+
+		case VAR_VOID_PTR:
+			this->void_ptr_=(void*)value;
+			break;
+
+		case VAR_OBJECT:
+			this->obj_=new_copy(value);
+			break;
+	}
+	this->type_=t;
+}
+
+const void* yaooc_variant_value(const_pointer p)
+{
+  yaooc_variant_const_pointer this=p;
+	const void* ret=NULL;
+	switch(this->type_) {
+  	case VAR_NONE:
+  		break;
+
+  	case VAR_CHAR:
+  	case VAR_UCHAR:
+  		ret = &this->ch_;
+  		break;
+
+  	case VAR_SHORT:
+  	case VAR_USHORT:
+  		ret = &this->sh_;
+  		break;
+
+  	case VAR_INT:
+  	case VAR_UINT:
+  		ret = &this->int_;
+  		break;
+
+  	case VAR_LONG:
+  	case VAR_ULONG:
+  		ret = &this->long_;
+  		break;
+
+  	case VAR_DOUBLE:
+  		ret = &this->dbl_;
+  		break;
+
+  	case VAR_CHAR_PTR:
+  		ret = this->chr_ptr_;
+  		break;
+
+  	case VAR_VOID_PTR:
+  		ret = this->void_ptr_;
+  		break;
+
+  	case VAR_OBJECT:
+  		ret = this->obj_;
+  		break;
+  }
+	return ret;
+}
+
+
+/* Class table definition for yaooc_variant */
+yaooc_variant_class_table_t yaooc_variant_class_table =
+{
+  .parent_class_table_ = (const class_table_t*) &yaooc_object_class_table,
+  .type_name_ = (const char*) "yaooc_variant_t",
+  .swap = (void(*)(pointer, pointer)) yaooc_variant_swap,
+  .type = (yaooc_variant_type_t(*)(const_pointer)) yaooc_variant_type,
+  .set = (void(*)(pointer, yaooc_variant_type_t,const void*)) yaooc_variant_set,
+  .value = (const void*(*)(const_pointer)) yaooc_variant_value,
+};
+
+/* Type info structure for yaooc_variant */
+DEFINE_TYPE_INFO(yaooc_variant,Y,Y,Y,Y,N,N,N,Y,yaooc_object);
+
+/* End YAOOCPP output */
+
